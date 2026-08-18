81451e167b45855c4d0944d04eae2a035e7abfab
afakhor <31812946+afakhor@users.noreply.github.com>
2026-08-18 12:19:19 +0700

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 allprojects {
     repositories {
         google()
@@ -11,18 +13,40 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-    // FIX NAMESPACE buat plugin lama kayak flutter_gl
+
     afterEvaluate {
+        // FIX 1: inject namespace buat flutter_gl yang belum ada
         if (project.hasProperty("android")) {
             val androidExt = project.extensions.findByName("android")
             if (androidExt is com.android.build.gradle.BaseExtension) {
                 if (androidExt.namespace == null) {
-                    androidExt.namespace = "com.${project.name.replace("-", "_").replace(" ", "_")}"
+                    androidExt.namespace = "com.${project.name.replace("-", "_")}"
+                }
+            }
+        }
+        // FIX 2: force kotlin version buat flutter_gl
+        if (project.name == "flutter_gl") {
+            project.extensions.configure<com.android.build.gradle.BaseExtension> {
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_1_8
+                    targetCompatibility = JavaVersion.VERSION_1_8
+                }
+            }
+        }
+    }
+
+    // FIX 3: force semua subproject pakai Kotlin 1.9.22
+    configurations.all {
+        resolutionStrategy {
+            eachDependency {
+                if (requested.group == "org.jetbrains.kotlin" && requested.name.contains("kotlin-gradle-plugin")) {
+                    useVersion("1.9.22")
                 }
             }
         }
     }
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
