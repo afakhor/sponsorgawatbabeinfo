ed18c060f7e733f19f786f35f27da3ea58a9798f
afakhor <31812946+afakhor@users.noreply.github.com>
2026-08-18 12:20:18 +0700

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,20 +3,15 @@ plugins {
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
 }
-
 android {
     namespace = "com.example.babe_info_fixed"
     compileSdk = 35
     ndkVersion = "25.1.8937393"
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-
+    kotlinOptions { jvmTarget = "1.8" }
     defaultConfig {
         applicationId = "com.example.babe_info_fixed"
         minSdk = 23
@@ -24,7 +19,6 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -33,9 +27,5 @@ android {
         }
     }
 }
-
-flutter {
-    source = "../.."
-}
-
+flutter { source = "../.." }
 dependencies {}
